e55d15ea41bf8af06f300f7bda86364ff3a1ac24
Dilum B <s220459698@deakin.edu.au>
2023-10-01 10:12:25 +1100

    UserStory 3.2 - amended furniture route to server.js

--- server.js
+++ server.js
@@ -3,39 +3,59 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const session = require("express-session");
+const furnitureRoutes = require("./routers/furnitureRoute");
+const furnitureModel = require("./models/furnitureModel");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 //database connection
-mongoose.connect(process.env.DB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const db = mongoose.connection;
 db.on("error", (error) => console.log(error));
-db.once("open", ()=> console.log("connected to the database!"));
+db.once("open", () => console.log("connected to the database!"));
 
-app.use(express.static('public'));
+app.use(express.static("public"));
+
+//add furniture route
+app.use("/api", furnitureRoutes);
+app.get("/furniture", async (req, res) => {
+  try {
+    // Fetch furniture data and pass it to the template
+    const furnitures = await furnitureModel.find();
+    res.render("furniture", { furnitures });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: true, message: "Internal Server Error" });
+  }
+});
 
 //middelwares
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(session({
-    secret: 'my secret key',
+app.use(
+  session({
+    secret: "my secret key",
     saveUninitialized: true,
-    resave: false
-}));
+    resave: false,
+  })
+);
 
-app.use((req, res, next) =>{
-    res.locals.message = req.session.message;
-    delete req.session.message;
-    next();
+app.use((req, res, next) => {
+  res.locals.message = req.session.message;
+  delete req.session.message;
+  next();
 });
 
 // set template engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 //route prefix
-app.use("", require('./routers/routes'));
+app.use("", require("./routers/routes"));
 
 app.listen(PORT, () => {
   console.log(`Server started at http://localhost:${PORT}`);
